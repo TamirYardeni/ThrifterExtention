378ac82d283d00e3eb59a46f7b79f6229ba1878e
TAMIR YARDENI <tamir.yardeni1@gmail.com>
2017-02-01 23:58:48 +0200

    Added ajax request to ws

--- ThrifterExtension/app/proceed-to-checkout.js
+++ ThrifterExtension/app/proceed-to-checkout.js
@@ -1,31 +1,38 @@
 ﻿// Checking page title
 if (document.title.indexOf("Amazon") != -1) {
-    debugger;
-    //Creating Elements
-    //Creating Elements
-    debugger;
-    document.addEventListener('submit', function (e) {
-        debugger;
+    document.addEventListener('submit', function (e) {   
         if (e.target.id == "gutterCartViewForm") {
 
             var arrayLength = document.getElementsByClassName('sc-list-body')[0].children.length;
 
             var items = [];
-            for (var counter = 0; counter < arrayLength - 1; counter++) {
+            for (var counter = 0; counter < arrayLength; counter++) {
                 var currItem = document.getElementsByClassName('sc-list-body')[0].children[counter];
-                var itemName = currItem.getElementsByClassName('sc-product-title')[0].innerHTML;
-                var itemPrice = currItem.getAttribute('data-price');
-                var itemImage = currItem.getElementsByClassName('sc-product-image')[0].getAttribute('src')
-                var cartItem = { 'name': itemName, 'price': itemPrice, 'imageLink': itemImage };
-                items.push(cartItem);
+                if (currItem.getElementsByClassName('sc-product-title')[0] != null) {
+                    var itemName = currItem.getElementsByClassName('sc-product-title')[0].innerHTML;
+                    var itemPrice = currItem.getAttribute('data-price');
+                    var itemImage = currItem.getElementsByClassName('sc-product-image')[0].getAttribute('src')
+                    var cartItem = { 'Name': itemName, 'Price': itemPrice, 'ImageLink': itemImage };
+                    items.push(cartItem);
+                }
             }
-
+            debugger;
             var dataToSend = {
                 "Email": "tamir.yardeni1@gmail.com",
                 "Product": items
             };
 
-            
+            jQuery.ajax({
+                type: "POST",
+                url: "https://thrifter.azurewebsites.net/api/ProductOwnerships",
+                data: items,
+                dataType: "json",
+                success: function (data) {
+                    console.log(data);
+                }
+            });
+
+            alert('a');
         }  
     });
 }
