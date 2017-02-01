04d3c099accc4423b41a982efc3ac49be27d1c13
Noa Segal <noasegal95@gmail.com>
2017-02-01 22:33:32 +0200

    added jquery

--- ThrifterExtension/app/product-page-script.js
+++ ThrifterExtension/app/product-page-script.js
@@ -1,9 +1,28 @@
-﻿// Checking page title
-if (document.title.indexOf("Google") != -1) {
+﻿debugger;
+// Checking page title
+if (document.title.indexOf("Amazon") != -1) {
     //Creating Elements
+    var div = document.createElement("DIV");
+    div.id = "dialog-div";
+
     var btn = document.createElement("BUTTON")
     var t = document.createTextNode("CLICK ME");
     btn.appendChild(t);
+    
+    btn.addEventListener('click', function (e) {
+
+        alert('helllllllllllloooooooo');
+        var s = '  <div class="alert alert-info"><strong>Info!</strong> Indicates a neutral informative change or action.</div>'; // HTML string
+        
+        div.innerHTML = s;
+        //$("#dialog-div").load("./products-popup.html").dialog(open);
+        /*$.post("product-popup.html", function (data) {
+
+            $("#dialog-div").html(data).fadeIn();
+
+        });*/
+    });
     //Appending to DOM 
-    document.body.appendChild(btn);
+    document.getElementById("title_feature_div").appendChild(btn);
+    document.getElementById("title_feature_div").appendChild(div);
 }
